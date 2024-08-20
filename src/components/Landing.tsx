e49e1f7a9b03b2724e3f5be70351de6bc3440675
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-08-21 02:15:14 +0530

    created(using ChatGPT) and added Y combinator logo and Backed by Y combinator text on the Landing Page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,67 +1,81 @@
 import { Box, Button, Typography } from "@mui/material";
 import { FillButton } from "./TopBar";
 import { ArrowRight } from "@mui/icons-material";
+import YCombinatorIcon from "../icons/YCombinatorIcon";
 
 export default function Landing() {
   return (
     <Box
-      display="flex"
-      flexDirection={{ xs: "column", md: "row" }}
-      justifyContent={"space-around"}
-      alignContent={"center"}
+      display={"flex"}
+      flexDirection={"column"}
+      gap={1}
       alignItems={"center"}
-      marginTop={2}
     >
       <Box
-        flexBasis={"500px"}
-        alignSelf={"center"}
-        paddingLeft={4}
-        display={"flex"}
-        flexDirection={"column"}
-        gap={2}
+        display="flex"
+        flexDirection={{ xs: "column", md: "row" }}
+        justifyContent={"space-around"}
+        alignContent={"center"}
+        alignItems={"center"}
+        marginTop={2}
       >
-        <Typography variant="h3" textAlign={"center"}>
-          Faster,
-          <br />
-          Accurate Loss Run Analysis
-        </Typography>
-        <Typography variant="subtitle1" textAlign={"center"}>
-          Streamline your underwriting process: convert unstructured submission
-          documents like ACORD and loss runs into actionable insights within 2
-          hours
-        </Typography>
-        <Box display={"flex"} justifyContent={"center"} gap={2}>
-          <FillButton color="inherit">
-            Book A Demo
-            <ArrowRight />
-          </FillButton>
-          <Button color="inherit" variant="outlined">
-            Contact Us
-          </Button>
+        <Box
+          flexBasis={"500px"}
+          alignSelf={"center"}
+          paddingLeft={4}
+          display={"flex"}
+          flexDirection={"column"}
+          gap={2}
+        >
+          <Typography variant="h3" textAlign={"center"}>
+            Faster,
+            <br />
+            Accurate Loss Run Analysis
+          </Typography>
+          <Typography variant="subtitle1" textAlign={"center"}>
+            Streamline your underwriting process: convert unstructured
+            submission documents like ACORD and loss runs into actionable
+            insights within 2 hours
+          </Typography>
+          <Box display={"flex"} justifyContent={"center"} gap={2}>
+            <FillButton color="inherit">
+              Book A Demo
+              <ArrowRight />
+            </FillButton>
+            <Button color="inherit" variant="outlined">
+              Contact Us
+            </Button>
+          </Box>
+        </Box>
+        <Box
+          style={{ width: 200 }}
+          sx={{ sm: { display: "none" }, md: { display: "block" } }}
+        />
+        <Box
+          position={"relative"}
+          width={{ xs: "300px", md: "800px" }}
+          height={{ height: "200px", md: 550 }}
+          margin={0}
+        >
+          <img
+            src="/images/banner-image.png"
+            alt="banner image"
+            width={"100%"}
+            style={{
+              display: "block",
+              position: "absolute",
+              right: 0,
+              top: "0%",
+              height: "100%",
+            }}
+          />
         </Box>
       </Box>
-      <Box
-        style={{ width: 200 }}
-        sx={{ sm: { display: "none" }, md: { display: "block" } }}
-      />
-      <Box
-        position={"relative"}
-        width={{ xs: "200px", md: "800px" }}
-        height={{ height: "120px", md: 550 }}
-        margin={0}
-      >
-        <img
-          src="/images/banner-image.png"
-          alt="banner image"
-          width={"100%"}
-          style={{
-            display: "block",
-            position: "absolute",
-            right: 0,
-            top: "0%",
-            height: "100%",
-          }}
-        />
+      <Box>
+        <Typography variant="h6" sx={{ display: "flex", alignItems: "center" }}>
+          Backed by&nbsp; <YCombinatorIcon />
+          &nbsp;Combinator
+        </Typography>
       </Box>
     </Box>
   );
---
+++ src/icons/YCombinatorIcon.tsx
@@ -0,0 +1,25 @@
+export default function YCombinatorIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        fill="currentColor"
+        style={{ verticalAlign: 'middle' }}
+      >
+        <rect x="0" y="0" width="24" height="24" rx="4" ry="4" fill="#888" />
+        <text
+          x="12"
+          y="16"
+          fontSize="14"
+          textAnchor="middle"
+          fill="#fff"
+          fontFamily="Arial"
+        >
+          Y
+        </text>
+      </svg>
+    );
+  }
+  
